21778a8e71490bc198d2b74d4feaeb83f7be8ade
Ivan Kirillov <ikirillov@mitre.org>
2014-11-10 16:11:35 -0500

    Initial commit

--- maec_to_stix.py
+++ maec_to_stix.py
@@ -15,13 +15,15 @@
 # MAEC to STIX Converter Script
 # Copyright 2014, MITRE Corp
 # v0.10 - BETA
-# Updated 10/27/2014
+# Updated 10/31/2014
 
 import sys
 import os
 import traceback
 import argparse
+import json
 from maec_to_stix.stix_wrapper import wrap_maec
+from maec_to_stix.indicator_extractor import IndicatorExtractor
 
 __version__ = 0.10
 
@@ -30,16 +32,28 @@ def main():
     parser = argparse.ArgumentParser(description="MAEC to STIX " + str(__version__))
     parser.add_argument("input", help="the name of the input MAEC Package XML file.")
     parser.add_argument("output", help="the name of the output STIX Package XML file.")
-    parser.add_argument("--wrap", "-w", help="wrap the input MAEC Package file in a STIX Package.", action="store_true", default=True)
-    #parser.add_argument("--extract", "-e", help="attempt to extract indicators from the MAEC Package file and output them in a new STIX Package.", action="store_true", default=False)
+    opts_group = parser.add_mutually_exclusive_group()
+    opts_group.add_argument("--wrap", "-w", help="wrap the input MAEC Package file in a STIX Package.", action="store_true", default=False)
+    opts_group.add_argument("--extract", "-e", help="attempt to extract indicators from the MAEC Package file and output them in a new STIX Package.", action="store_true", default=False)
     args = parser.parse_args()
 
+    # Wrap the MAEC document in a STIX Package
     if args.wrap:
         stix_package = wrap_maec(args.input, __version__)
-        out_file = open(args.output, "w")
-        out_file.write(stix_package.to_xml())
-        out_file.flush()
-        out_file.close()
+    # Attempt to extract Indicators from the MAEC document
+    if args.extract:
+        # Parse and load the JSON config structure
+        with open('extractor_config.json', mode='r') as f:
+            extractor_config = json.loads(f.read())
+        extractor = IndicatorExtractor(args.input, extractor_config, __version__)
+        stix_package = extractor.create_stix()
+
+    # Write out the STIX Package to an XML file
+    out_file = open(args.output, "w")
+    out_file.write(stix_package.to_xml())
+    out_file.flush()
+    out_file.close()
+
 
 if __name__ == "__main__":
     main()    
---
+++ maec_to_stix/indicator_extractor.py
@@ -0,0 +1,284 @@
+# MAEC Indicator Extractor Classes
+# For extracting Indicators from MAEC data
+
+import json
+import os
+import collections
+import maec
+from maec.bundle.bundle import Bundle, ObjectList
+import cybox.utils
+from cybox.core import Object
+from cybox.common import (ObjectProperties, ToolInformation, 
+                          ToolInformation, ToolInformationList)
+import stix.utils
+from stix.core import STIXHeader, STIXPackage
+from stix.common import Confidence, InformationSource
+from stix.indicator import Indicator
+
+class IndicatorExtractor(object):
+    def __init__(self, input_file, config, version):
+        # The input MAEC Package file
+        self.input_file = input_file
+        # JSON configuration blob
+        self.config = config
+        # Tool version
+        self.version = version
+        # Dictionary of candidate Objects to be converted into Indicators
+        self.candidate_indicator_objects = {}
+        # List of final Objects to be converted into Indicators
+        self.final_indicator_objects = []
+        # List of supported Actions
+        self.supported_actions = []
+        # Dictionary of supported Objects and their properties
+        self.supported_objects = {}
+        # Set the STIX namsespace and alias
+        stix.utils.set_id_namespace({'https://github.com/MAECProject/maec-to-stix' : 'maecToSTIX'})
+        # Set the CybOX namespace and alias
+        cybox.utils.set_id_namespace(cybox.utils.Namespace('https://github.com/MAECProject/maec-to-stix' , 'maecToSTIX'))
+        # Parse the config structure
+        self.parse_config()
+        # Parse the MAEC Package
+        self.parse_package()
+        # Prune and deduplicate the initial candidate Indicator Objects
+        self.prune_objects()
+        # Prepare the Objects for Indicator conversion
+        self.prepare_objects()
+
+
+    def create_stix_indicator(self, object):
+        """Create and return a STIX Indicator for a single input Object."""
+        indicator = Indicator()
+        indicator.title = "Malware Artifact Extracted from MAEC Document"
+        indicator.add_indicator_type("Malware Artifacts")
+        indicator.add_observable(object.properties)
+        # Set the proper Confidence on the Indicator
+        confidence = Confidence()
+        confidence.value = "Low"
+        confidence.description = "Tool-generated Indicator. It is HIGHLY recommended that it be vetted by a human analyst before usage."
+        indicator.confidence = confidence
+        return indicator
+        
+    def create_stix(self):
+        """Create and return a STIX Package with the final Indicator Objects."""
+        stix_package = STIXPackage()
+        stix_header = STIXHeader()
+        stix_header.add_package_intent("Indicators - Malware Artifacts")
+        stix_header.title = "STIX Indicators extracted from MAEC file: " + str(self.input_file)
+        # Add the Information Source to the STIX Header
+        tool_info = ToolInformation()
+        stix_header.information_source = InformationSource()
+        tool_info.name = "MAEC to STIX"
+        tool_info.version = str(self.version)
+        stix_header.information_source.tools = ToolInformationList(tool_info)
+        stix_package.stix_header = stix_header
+        # Create and add a STIX Indicator for each Object
+        for object in self.final_indicator_objects:
+            indicator = self.create_stix_indicator(object)
+            stix_package.add_indicator(indicator)
+        return stix_package
+
+    def set_object_property(self, property, condition = "Equals"):
+        """Add a condition to an Object property and all of its children."""
+        if isinstance(property, str):
+            property = {'value':property, 'condition':condition}
+        elif isinstance(property, dict):
+            # Test if we're dealing with a "value" dictionary
+            # or a data structure dictionary
+            if 'value' in property:
+                property['condition'] = condition
+            else:
+                for key, value in property.items():
+                    property[key] = self.set_object_property(value, condition)
+        elif isinstance(property, list):
+            for item in property:
+                self.set_object_property(item, condition)
+        return property
+
+    def prepare_objects(self):
+        """Prepare the final Indicator Objects for translation into STIX Indicators.
+           Set their condition attributes as appropriate."""
+        for object in self.final_indicator_objects:
+            object_xsi_type = object.properties._XSI_TYPE
+            object_properties_dict = object.properties.to_dict()
+            updated_properties_dict = {}
+            for property_name, property_value in object_properties_dict.items():
+                updated_properties_dict[property_name] = self.set_object_property(property_value)
+            updated_properties_dict['xsi:type'] = object_xsi_type
+            object.properties = ObjectProperties.from_dict(updated_properties_dict)
+
+    def prune_object_properties(self, object_dict, supported_properties, parent_key = None):
+        """Prune any un-wanted properties from a single Object.
+           Return a dictionary with only the allowed properties."""
+        pruned_dict = {}
+        for property_name, property_value in object_dict.items():
+            # Keep any xsi:type values
+            if property_name == "xsi:type":
+                pruned_dict["xsi:type"] = property_value
+                continue
+            if parent_key:
+                updated_key = parent_key + "/" + property_name
+            else:
+                updated_key = property_name
+            # Test if the value is a string
+            # All paths lead to Rome (or strings)  
+            if isinstance(property_value, basestring):
+                if not parent_key and property_name in supported_properties:
+                    pruned_dict[property_name] = property_value
+                elif parent_key:
+                    split_key = parent_key.split("/")
+                    split_key.append(property_name)
+                    for object_path in supported_properties:
+                        split_object_path = object_path.split("/")
+                        # Test to make sure the root keys match
+                        if split_key[0] == split_object_path[0]:
+                            match = True
+                            # Corner case for dealing with "value" keys
+                            # that may appear in element dictionaries
+                            if split_key[-1] == "value":
+                                split_key.pop()
+                            # Test to make sure the other path keys are 
+                            # encompassed by the supported object path
+                            for path_value in split_key[1:]:
+                                if path_value not in split_object_path[1:]:
+                                    match = False
+                                    break
+                            # Add the property key/value if everything matched
+                            if match:
+                                pruned_dict[property_name] = property_value
+            # Test if the value is a dictionary
+            elif isinstance(property_value, dict):
+                pruned_nested_dict = {}
+                pruned_nested_dict = self.prune_object_properties(property_value, supported_properties, updated_key)
+                if pruned_nested_dict:
+                    pruned_dict[property_name] = pruned_nested_dict
+            # Test if the value is a list
+            elif isinstance(property_value, list):
+                pruned_list = []
+                for list_item in property_value:
+                    pruned_list.append(self.prune_object_properties(list_item, supported_properties, updated_key))
+                if pruned_list:
+                    pruned_dict[property_name] = pruned_list
+        return pruned_dict
+
+    def prune_objects(self):
+        """Prune any un-wanted properties from the Candidate Indicator Objects.
+           Also, deduplicate any identical Objects."""
+        # Prune any unwanted properties from Objects
+        for action_name, object_list in self.candidate_indicator_objects.items():
+            for object in object_list:
+                xsi_type = object.properties._XSI_TYPE
+                pruned_properties = self.prune_object_properties(object.properties.to_dict(), self.supported_objects[xsi_type])
+                if pruned_properties:
+                    pruned_properties["xsi:type"] = xsi_type
+                    # Create a new Object with the pruned ObjectProperties
+                    pruned_object = Object()
+                    pruned_object.properties = ObjectProperties.from_dict(pruned_properties)
+                    # Add the new Object to the final list of Indicators
+                    self.final_indicator_objects.append(pruned_object)
+        # Deduplicate any identical Objects
+        temp_bundle = Bundle()
+        temp_bundle.objects = ObjectList(self.final_indicator_objects)
+        temp_bundle.deduplicate()
+        self.final_indicator_objects = temp_bundle.get_all_objects()
+
+    def flatten_dict(self, d, parent_key='', sep='/'):
+        """Flatten an input dictionary."""
+        items = []
+        for k, v in d.items():
+            new_key = parent_key + sep + k if parent_key else k
+            if isinstance(v, collections.MutableMapping):
+                items.extend(self.flatten_dict(v, new_key, sep=sep).items())
+            elif isinstance(v, list):
+                for list_item in v:
+                    items.extend(self.flatten_dict(list_item, new_key, sep=sep).items())
+            else:
+                items.append((new_key, v))
+        return dict(items)
+
+    def parse_object_config_dict(self, object_type, config_dict):
+        """Parse an Object configuration dictionary."""
+        flattened_dict = self.flatten_dict(config_dict)
+        for key, value in flattened_dict.items():
+            if value:
+                if object_type not in self.supported_objects:
+                    self.supported_objects[object_type] = [key]
+                else:
+                    self.supported_objects[object_type].append(key)
+
+    def parse_granular_config(self, granular_config_file):
+        """Parse a granular JSON configuration structure."""
+        with open(os.path.join("config",granular_config_file), mode='r') as f:
+            config = json.loads(f.read())
+        for config_type, config_values in config.items():
+            if config_type == "supported objects":
+                for object_type, properties_dict in config_values.items():
+                    self.parse_object_config_dict(object_type, properties_dict)
+            elif config_type == "supported actions":
+                for enum_name, actions_dict in config_values.items():
+                    for action_name, enabled in actions_dict.items():
+                        if enabled:
+                            self.supported_actions.append(action_name)
+        print self.supported_objects
+
+    def parse_config(self):
+        """Parse and break up the JSON configuration structure."""
+        # Use the granular options structure if specified
+        if self.config["use_granular_options"]:
+            self.parse_granular_config("granular_config.json")
+        else:
+            abstracted_options = self.config["abstracted_options"]
+            for option, enabled in abstracted_options.items():
+                if option == "file_system_activity" and enabled:
+                    self.parse_granular_config("file_system_activity_config.json")
+                elif option == "registry_activity" and enabled:
+                    self.parse_granular_config("registry_activity_config.json")
+                elif option == "mutex_activity" and enabled:
+                    self.parse_granular_config("mutex_activity_config.json")           
+                elif option == "process_activity" and enabled:
+                    self.parse_granular_config("process_activity_config.json")
+                elif option == "service_activity" and enabled:
+                    self.parse_granular_config("service_activity_config.json")
+                elif option == "network_activity" and enabled:
+                    self.parse_granular_config("network_activity_config.json")
+                elif option == "driver_activity" and enabled:
+                    self.parse_granular_config("driver_activity_config.json")
+        #print self.supported_objects    
+
+    def parse_package(self):
+        """Parse and extract Indicators from a MAEC Package"""
+        maec_package = maec.parse_xml_instance(self.input_file)['api']
+        if maec_package.malware_subjects:
+            for malware_subject in maec_package.malware_subjects:
+                self.parse_malware_subject(malware_subject)
+
+    def parse_action(self, action, action_name):
+        """Parse and extract Indicators from a MAEC Malware Action"""
+        if action.associated_objects:
+            for associated_object in action.associated_objects:
+                if associated_object.properties:
+                    object_xsi_type = associated_object.properties._XSI_TYPE
+                    properties_dict = associated_object.properties.to_dict()
+                    if object_xsi_type in self.supported_objects:
+                        for property in properties_dict:
+                            if property in self.supported_objects[object_xsi_type]:
+                                self.candidate_indicator_objects[action_name].append(associated_object)
+                                break
+
+    def parse_bundle(self, bundle, mal_inst_obj):
+        """Parse and extract Indicators from a MAEC Bundle"""
+        # Dereference the Objects in the Bundle to make it easier to parse
+        bundle.dereference_objects(extra_objects=[mal_inst_obj])
+        binned_actions = bundle.get_all_actions(bin=True)
+        for action_name, actions in binned_actions.items():
+            if action_name in self.supported_actions:
+                if action_name not in self.candidate_indicator_objects:
+                    self.candidate_indicator_objects[action_name] = []
+                for action in actions:
+                    self.parse_action(action, action_name)
+
+    def parse_malware_subject(self, malware_subject):
+        """Parse and extract Indicators from a Malware Subject"""
+        # Parse the Findings Bundles in the Malware Subject
+        if malware_subject.findings_bundles and malware_subject.findings_bundles.bundle:
+            for bundle in malware_subject.findings_bundles.bundle:
+                self.parse_bundle(bundle, malware_subject.malware_instance_object_attributes)
