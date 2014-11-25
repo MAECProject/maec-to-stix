de188552a65b9b130d4557f2a2955442e7928178
Ivan Kirillov <ikirillov@mitre.org>
2014-11-25 09:42:35 -0500

    Added wrap_maec_package and extract_indicators methods

--- maec_to_stix/__init__.py
+++ maec_to_stix/__init__.py
@@ -3,5 +3,42 @@
 
 __version__ = "1.0.0-alpha1"
 
+import maec
 from stix_wrapper import wrap_maec
 from indicator_extractor import IndicatorExtractor
+
+def wrap_maec_package(package_filename):
+    """Wrap a MAEC Package file in a STIX Package/TTP.
+    
+    Args:
+        package_filename: The name of the MAEC Package file to wrap.
+
+    Returns:
+        A ``stix.STIXPackage`` instance with the wrapped MAEC data.
+    
+    """
+    # Parse the input MAEC Package
+    maec_package = maec.parse_xml_instance(package_filename)['api']
+
+    # Wrap the MAEC Package in STIX
+    stix_package = wrap_maec(maec_package, package_filename)
+
+    return stix_package
+
+def extract_indicators(package_filename):
+    """Extract STIX Indicators from a MAEC Package file.
+    
+    Args:
+        package_filename: The name of the MAEC Package file to extract indicators from.
+
+    Returns:
+        A ``stix.STIXPackage`` instance with the extracted STIX Indicators.
+    
+    """
+    # Parse the input MAEC Package
+    maec_package = maec.parse_xml_instance(package_filename)['api']
+
+    # Extract the STIX Indicators from the MAEC Package
+    indicator_extractor = IndicatorExtractor(maec_package, package_filename)
+    
+    return indicator_extractor.stix_package
