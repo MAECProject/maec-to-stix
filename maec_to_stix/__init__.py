cfb122aea76a14142333833fa63f568265f0c00d
Ivan Kirillov <ikirillov@mitre.org>
2014-12-09 15:57:56 -0500

    Updated parameter names and docstrings in wrap_maec and extract_indicators to reflect ability to pass in file-like objects

--- maec_to_stix/__init__.py
+++ maec_to_stix/__init__.py
@@ -3,6 +3,7 @@
 
 __version__ = "1.0.0-alpha1"
 
+import os
 import maec
 from maec_to_stix.stix_wrapper import wrap_maec
 from maec_to_stix.indicator_extractor import IndicatorExtractor
@@ -12,29 +13,34 @@ def _custom_formatwarning(msg, *a):
     # Ignore everything except the message
     return "Warning: " + str(msg) + '\n'
 
-def wrap_maec_package(package_filename):
+def wrap_maec_package(package):
     """Wrap a MAEC Package file in a STIX Package/TTP.
     
     Args:
-        package_filename: The name of the MAEC Package file to wrap.
+        package: The MAEC Package file or file-like object to wrap.
 
     Returns:
         A ``stix.STIXPackage`` instance with the wrapped MAEC data.
     
     """
     # Parse the input MAEC Package
-    maec_package = maec.parse_xml_instance(package_filename)['api']
+    maec_package = maec.parse_xml_instance(package)['api']
+
+    # Test if the MAEC Package is a filename or not
+    package_filename = None
+    if isinstance(package, basestring) and os.path.isfile(package):
+        package_filename = package
 
     # Wrap the MAEC Package in STIX
     stix_package = wrap_maec(maec_package, package_filename)
 
     return stix_package
 
-def extract_indicators(package_filename, config_directory=None):
+def extract_indicators(package, config_directory=None):
     """Extract STIX Indicators from a MAEC Package file.
     
     Args:
-        package_filename: The name of the MAEC Package file to extract indicators from.
+        package: The MAEC Package file or file-like object to wrap.
         config_directory: (optional) The path to the directory housing the indicator 
             extraction configuration files. 
 
@@ -45,7 +51,12 @@ def extract_indicators(package_filename, config_directory=None):
     
     """
     # Parse the input MAEC Package
-    maec_package = maec.parse_xml_instance(package_filename)['api']
+    maec_package = maec.parse_xml_instance(package)['api']
+
+    # Test if the MAEC Package is a filename or not
+    package_filename = None
+    if isinstance(package, basestring) and os.path.isfile(package):
+        package_filename = package
 
     # Extract the STIX Indicators from the MAEC Package
     indicator_extractor = IndicatorExtractor(maec_package, package_filename, config_directory)
